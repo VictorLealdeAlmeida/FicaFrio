ab917eaa897d827af9146adc82c14411ad6ad907
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor-2.local>
2016-09-09 12:07:04 -0300

    Sicronizacao das acoes com o circulo

--- FicaFrio/CurrentStepViewController.m
+++ FicaFrio/CurrentStepViewController.m
@@ -13,9 +13,12 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *circleView;
 @property (weak, nonatomic) IBOutlet UILabel *circleNumber;
+@property (weak, nonatomic) IBOutlet UILabel *labelStep;
 
 - (IBAction)circleButton:(id)sender;
 - (void) changeNumber;
+
+
 
 @end
 
@@ -23,9 +26,14 @@
 
 int numberStep = 1;
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *steps;
+    steps = [NSArray arrayWithObjects: @") contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos", @"Because we used the NSArray class in the above example the contents of the array object cannot be changed subsequent to initialization.", @"The objects contained in an array are given index positions beginning at position zero. Each element may be accessed by passing the required index position through as an argument to the NSArray objectAtIndex", @"Yellow", nil];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,7 +42,11 @@ int numberStep = 1;
 }
 
 - (IBAction)circleButton:(id)sender {
+
+    [self changeNumber];
     [self.circleView rotation: 1.0 option:0];
+    _labelStep.text = [NSString stringWithFormat: @"%s", "steps[1]"];
+
 }
 
 - (void) changeNumber{
