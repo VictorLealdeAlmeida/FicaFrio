1f78dac9e55dc44102435346f42523a2e541325e
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor-2.local>
2016-09-19 09:44:08 -0300

    Com watch

---
+++ FicaFrioWatch Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  FicaFrioWatch Extension
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 19/09/16.
+//  Copyright © 2016 PokeGroup. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ FicaFrioWatch Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  FicaFrioWatch Extension
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 19/09/16.
+//  Copyright © 2016 PokeGroup. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
