672332ee95b6c842d55e13f8e8fc77093f64ec6b
Diana <monteiro.diana18@gmail.com>
2016-09-18 22:21:09 -0300

    Info.plist com NSCameraUsageDescription

--- FicaFrio/SetGoalViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SetGoalViewController.swift
-//  FicaFrio
-//
-//  Created by Victor Leal Porto de Almeida Arruda on 06/09/16.
-//  Copyright © 2016 Victor Leal Porto de Almeida Arruda. All rights reserved.
-//
-
-import Foundation
