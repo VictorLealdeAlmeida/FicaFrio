0119a34aff441cdd3cd0bc1e7717e47612291b68
Bárbara Souza <btas@bepid.cin.ufpe.br>
2016-09-21 16:22:50 -0300

    começo da tela de relaxamento wacth

---
+++ FicaFrioWatch Extension/RelaxInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  RelaxInterfaceController.h
+//  FicaFrio
+//
+//  Created by Bárbara Souza on 21/09/16.
+//  Copyright © 2016 PokeGroup. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface RelaxInterfaceController : WKInterfaceController
+
+@end
---
+++ FicaFrioWatch Extension/RelaxInterfaceController.m
@@ -0,0 +1,39 @@
+//
+//  RelaxInterfaceController.m
+//  FicaFrio
+//
+//  Created by Bárbara Souza on 21/09/16.
+//  Copyright © 2016 PokeGroup. All rights reserved.
+//
+
+#import "RelaxInterfaceController.h"
+
+@interface RelaxInterfaceController ()
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceImage *relaxImage;
+
+@end
+
+@implementation RelaxInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    [_relaxImage setImageNamed:@"respirar"];
+    [_relaxImage startAnimatingWithImagesInRange: NSMakeRange(1, 11) duration:0.5 repeatCount:3];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
